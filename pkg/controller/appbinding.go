b526ccd8712c5ec97144d9da1f79efb26bc106b8
Md. Emruz Hossain <emruz@appscode.com>
2019-08-10 07:40:05 +0600

    Set database version in AppBinding (#7)

--- pkg/controller/appbinding.go
+++ pkg/controller/appbinding.go
@@ -51,11 +51,17 @@ func (c *Controller) ensureAppBinding(db *api.PerconaXtraDB) (kutil.VerbType, er
 		return kutil.VerbUnchanged, err
 	}
 
+	pxVersion, err := c.ExtClient.CatalogV1alpha1().PerconaXtraDBVersions().Get(string(db.Spec.Version), metav1.GetOptions{})
+	if err != nil {
+		return kutil.VerbUnchanged, fmt.Errorf("failed to get PerconaXtraDBVersion %v for %v/%v. Reason: %v", db.Spec.Version, db.Namespace, db.Name, err)
+	}
+
 	_, vt, err := appcat_util.CreateOrPatchAppBinding(c.AppCatalogClient, meta, func(in *appcat.AppBinding) *appcat.AppBinding {
 		core_util.EnsureOwnerReference(&in.ObjectMeta, ref)
 		in.Labels = db.OffshootLabels()
 
 		in.Spec.Type = appmeta.Type()
+		in.Spec.Version = pxVersion.Spec.Version
 		in.Spec.ClientConfig.URL = types.StringP(fmt.Sprintf("tcp(%s:%d)/", db.ServiceName(), defaultDBPort.Port))
 		in.Spec.ClientConfig.Service = &appcat.ServiceReference{
 			Scheme: "mysql",
