f4dc6ce65eb7c732ce8b00c74771221f9ee4b73c
Tamal Saha <tamal@appscode.com>
2020-11-22 09:02:32 -0800

    Format shell scripts
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/gendocs/make.sh
+++ hack/gendocs/make.sh
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
 pushd $GOPATH/src/kubedb.dev/proxysql/hack/gendocs
 go run main.go
 popd
