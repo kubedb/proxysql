3e30741191158022cc2f71677b03cd4d5603a0e4
Tamal Saha <tamal@appscode.com>
2021-06-15 17:45:29 -0700

    Send audit events if analytics enabled (#177)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/controller/config.go
+++ pkg/controller/config.go
@@ -30,6 +30,7 @@ import (
 	"k8s.io/client-go/tools/record"
 	reg_util "kmodules.xyz/client-go/admissionregistration/v1beta1"
 	"kmodules.xyz/client-go/discovery"
+	"kmodules.xyz/client-go/tools/cli"
 )
 
 const (
@@ -66,7 +67,7 @@ func (c *OperatorConfig) New() (*Controller, error) {
 	// audit event publisher
 	// WARNING: https://stackoverflow.com/a/46275411/244009
 	var auditor cache.ResourceEventHandler
-	if c.LicenseFile != "" {
+	if c.LicenseFile != "" && cli.EnableAnalytics {
 		natscfg, err := auditlib.NewNatsConfig(c.KubeClient.CoreV1().Namespaces(), c.LicenseFile)
 		if err != nil {
 			return nil, err
